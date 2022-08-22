e70d21cd507ce4548e9c89fa69fd2bfc9d60acea
Andrea Leone <106384505+LeoneAndrea@users.noreply.github.com>
2022-08-22 11:34:29 +0200

    Update deriv4.m

--- src/control_functions/deriv4.m
+++ src/control_functions/deriv4.m
@@ -1,4 +1,13 @@
 function u = deriv4(v,vdot,v2dot,v3dot,v4dot)
+% support function used to define the control functions
+%
+% :param v: vector 
+% :param vdot: time derivative of vector v  
+% :param v2dot: second time derivative of vector v  
+% :param v3dot: third time derivative of vector v  
+% :param v4dot: fourth time derivative of vector v  
+
+% :returns: time derivative of the output of deriv3
 
     norma = vecnorm(v);
     norma1d = (vdot'*v)/norma;
@@ -31,4 +40,3 @@ function u = deriv4(v,vdot,v2dot,v3dot,v4dot)
 %           2*vdot*norma1d^3 + 6*v*norma2d*norma1d^2)/norma^16;
     
 end
-    
