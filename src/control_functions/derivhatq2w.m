f3a23a04004c56c6a7d5318047ad961049d68c1f
Andrea Leone <106384505+LeoneAndrea@users.noreply.github.com>
2022-08-22 11:35:29 +0200

    Update derivhatq2w.m

--- src/control_functions/derivhatq2w.m
+++ src/control_functions/derivhatq2w.m
@@ -1,5 +1,6 @@
 function u=derivhatq2w(q,w,qdot,wdot)
-     
+% support function used to define the control functions
+
     % u = (q'*w)*q - (q'*q)*w
     
     u = (qdot'*w + q'*wdot)*q + (q'*w)*qdot - 2*(q'*qdot)*w - (q'*q)*wdot;
