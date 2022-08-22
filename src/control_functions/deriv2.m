e9a45bf19ebc2663e37729bc116ab13b000ca710
Andrea Leone <106384505+LeoneAndrea@users.noreply.github.com>
2022-08-22 11:32:38 +0200

    Update deriv2.m

--- src/control_functions/deriv2.m
+++ src/control_functions/deriv2.m
@@ -1,6 +1,13 @@
 function u = deriv2(v,vdot,v2dot)
+% support function used to define the control functions
+%
+% :param v: vector 
+% :param vdot: time derivative of vector v  
+% :param v2dot: second time derivative of vector v  
+%
+% :returns: time derivative of the output of deriv1
 
-    % recall deriv1:   u = (vdot*norma-v*normadot)/norma^2;
+% recall deriv1:   u = (vdot*norma-v*normadot)/norma^2;
 
     norma = vecnorm(v);
     norma1d = (vdot'*v)/norma;
@@ -9,6 +16,6 @@ function u = deriv2(v,vdot,v2dot)
     u = 1/norma^3*(-2*vdot*norma*norma1d + norma^2*v2dot - norma*v*norma2d +...
         2*v*norma1d^2);
     
-%     u = (v2dot*norma^3-v*norma2d*norma^2-2*vdot*norma1d*norma^2+2*v*norma1d^2*norma)/norma^4;
+%   u = (v2dot*norma^3-v*norma2d*norma^2-2*vdot*norma1d*norma^2+2*v*norma1d^2*norma)/norma^4;
 
 end
