fcceffd31129a01a7d58be3b9d598c516e639f76
Andrea Leone <106384505+LeoneAndrea@users.noreply.github.com>
2022-08-22 11:31:58 +0200

    Update deriv1.m

--- src/control_functions/deriv1.m
+++ src/control_functions/deriv1.m
@@ -1,4 +1,10 @@
 function u = deriv1(v,vdot)
+% support function used to define the control functions
+%
+% :param v: vector  
+% :param vdot: time derivative of vector v
+%
+% :returns: time derivative of the vector v divided by the norm of v
 
     norma = vecnorm(v);
     normadot = (vdot'*v)/norma;
